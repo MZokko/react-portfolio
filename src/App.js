f237ca565135c2818bac2d73e17dfae6f5bb85cc
alexandre <akekiwy@gmail.com>
2020-12-14 14:49:20 +1100

    grid styling

--- src/App.js
+++ src/App.js
@@ -3,18 +3,31 @@ import Content from './Components/Content';
 import Resume from './Components/resume/index';
 import Portefolio from './Components/portefolio/index'
 import './App.css';
+import { Container, Grid } from '@material-ui/core';
 
 
 function App() {
   return (
-    <div className='App'>
-      <AboutMe />
-      <div>
+    <Container>
+      <Grid container>
+        <Grid item xs={12} sm={12} md={4} lg={3} >
+          <AboutMe/>
+        </Grid>
+        <Grid item xs >
+          {/* <Header/> */}
           <Portefolio/>
-          <Resume />
-      </div>
+          <Resume/>
+        </Grid>
+      </Grid>
+    </Container>
+    // <div className='App'>
+    //   <AboutMe />
+    //   <div>
+    //       <Portefolio/>
+    //       <Resume />
+    //   </div>
       
-    </div>
+    // </div>
   );
 }
 
