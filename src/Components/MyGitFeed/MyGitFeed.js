735260a6a5cda29361d40ec63705131a3122d26d
alexandre <akekiwy@gmail.com>
2021-03-22 13:48:09 +1100

    ui update

--- src/Components/MyGitFeed/MyGitFeed.js
+++ src/Components/MyGitFeed/MyGitFeed.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { Card, Grid } from '@material-ui/core';
 function MyGitFeed() {
-    return (
-        <>
-            <Grid>
-                <Grid  item className='section_title mb_30'>
-                <span></span>
-        <h6 className='section_title_text'>Github activity</h6>
-                </Grid>
-            </Grid>
-        </>
-    )
+  return (
+    <>
+      <Grid>
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h6 className='section_title_text'>Github activity</h6>
+        </Grid>
+      </Grid>
+      <Grid container spacing={3} className='gridGiTCard'></Grid>
+    </>
+  );
 }
 
-export default MyGitFeed
+export default MyGitFeed;
