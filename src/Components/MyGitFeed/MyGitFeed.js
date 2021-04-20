7b732cf85e6186fecb95cdbf6d1b35d25f4b47d0
alexandre <akekiwy@gmail.com>
2021-04-20 17:05:05 +1000

    Update MyGitFeed.js
    tryout the github api response

--- src/Components/MyGitFeed/MyGitFeed.js
+++ src/Components/MyGitFeed/MyGitFeed.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React,  { useEffect, useState } from 'react';
 import { Card, Grid } from '@material-ui/core';
-function MyGitFeed() {
+
+const MyGitFeed = () => {
+
+  //loading fetch data from git api
+  const  githubApiFetch = async()=>{
+    const url= 'https://api.github.com/users/{MZokko}'
+    const response =  await fetch(url);
+    const result = await response.json();
+    console.log(result)
+  }
+
+  useEffect(() => {
+    githubApiFetch();
+  }, [])
+
+
   return (
     <>
       <Grid>
@@ -12,6 +27,6 @@ function MyGitFeed() {
       <Grid container spacing={3} className='gridGiTCard'></Grid>
     </>
   );
-}
+};
 
 export default MyGitFeed;
