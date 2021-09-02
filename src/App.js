68665951c3b61c926891f02536bc51f9f9ccc250
alexandre <akekiwy@gmail.com>
2021-09-02 13:47:32 +1000

    fix import

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Portefolio from './Components/portefolio';
 import Header from './Components/Header/Header';
 import ThemeToggle from './Components/ThemeToggle'
 import LandingPage from './Components/LandingPage/LandingPage';
-import { Container, Grid } from '@material-ui/core';
+import {Grid } from '@material-ui/core';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Flip from 'react-reveal/Flip';
 //o
