3f733fe59791ae4e4087a63dafa274678cec5441
alexandre <akekiwy@gmail.com>
2022-05-02 15:45:49 +1000

    spaces fix

--- src/Components/UI/containerMainStyle.js
+++ src/Components/UI/containerMainStyle.js
@@ -1,25 +1,23 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const containerMainStyle = styled.div`
   display: flex;
-  flex-direction:row;
+  flex-direction: row;
   justify-content: space-between;
   align-items: flex-start;
   /* background-color: aqua; */
   width: 100vw;
-  height:100%; 
+  height: 100vh;
   background-color: ${(props) => props.theme.background};
   /* padding-top: 60px; */
   /* padding-bottom: 60px;  */
 
-  @media only screen and (max-width: 800px){
-    
+  @media only screen and (max-width: 800px) {
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-content: center;
     padding-top: 30px;
-
   }
 `;
 
